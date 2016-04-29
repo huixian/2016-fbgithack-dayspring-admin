9bceb74f47130a368355339b8fdcb9bed89d6869
Goh Khoon Hiang <gohkhoonhiang@gmail.com>
2016-04-30 04:00:26 +0800

    Service for getting target vs actual donation amounts in given year

--- dsadmin/analysis/services.py
+++ dsadmin/analysis/services.py
@@ -22,3 +22,16 @@ def __get_donation_by_year_month(year, month):
                 total_amt += donation['DonationAmount']
     return total_amt
 
+def get_donation_target_by_year(year):
+    months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
+    # targets should be retrieved from a central datasource, but we will mock it up for the time being
+    targets = [100000,100000,100000,100000,100000,100000,100000,100000,100000,100000,100000,100000]
+    actuals = []
+    for month, month_name in enumerate(months):
+        total_amt = __get_donation_by_year_month(year, month)
+        actuals.append(total_amt)
+    months.insert(0, "Caption")
+    targets.insert(0, "Target")
+    actuals.insert(0, "Actual")
+    return [months, targets, actuals]
+    
