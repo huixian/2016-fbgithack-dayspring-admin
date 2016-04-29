1c538dbc3872a057ce666b26cd39a9ff20fdb778
Goh Khoon Hiang <gohkhoonhiang@gmail.com>
2016-04-30 06:35:14 +0800

    Increase target amount for better data visualization

--- dsadmin/analysis/services.py
+++ dsadmin/analysis/services.py
@@ -34,7 +34,7 @@ def __get_last_donation_data_in_days(days):
 def get_donation_target_by_year(year):
     months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
     # targets should be retrieved from a central datasource, but we will mock it up for the time being
-    targets = [100000,100000,100000,100000,100000,100000,100000,100000,100000,100000,100000,100000]
+    targets = [500000,500000,500000,500000,500000,500000,500000,500000,500000,500000,500000,500000]
     actuals = []
     for month, month_name in enumerate(months):
         total_amt = __get_donation_by_year_month(year, month+1)
